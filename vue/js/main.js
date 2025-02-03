8afeea28a145cfd3e4d18fcab6b5f822cc763066
tohruss <taraskind6@gmail.com>
2025-02-03 13:44:42 +0700

    практическая3

--- vue/js/main.js
+++ vue/js/main.js
@@ -5,7 +5,10 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         description: "A pair of warm, fuzzy socks.",
         altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        inventory: 100,
+        onSale: true
+
     }
 })
 
